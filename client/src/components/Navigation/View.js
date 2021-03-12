fc8ae6ea99852e6647bc616fd43c0d4c7ef5fbb2
ClintFix <clint.fix@gmail.com>
2021-03-12 12:40:32 -0700

    basic navigation bar working

--- client/src/components/Navigation/View.js
+++ client/src/components/Navigation/View.js
@@ -3,15 +3,33 @@ import styled from 'styled-components'
 
 //Styling - Styled Components
 const NavBar = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 44px;
     background-color: lightgray;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 15px 0px;
+`
+
+const PostButton = styled.div`
+    background-color: #ffffff;
+    font-weight: 700;
+    border-radius: 8px;
+    width: 67px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 export default function View() {
     return(
         <NavBar>
-            Hello, World!
+            <div>
+                PodChat
+            </div>
+            <PostButton>Post</PostButton>
         </NavBar>
     )
 }
