29aa2ee023a8c9ad8e06680c3a43e6388ef527e3
ClintFix <clint.fix@gmail.com>
2021-03-27 09:07:22 -0600

    added author to comments

--- client/src/components/Comments/Comment.js
+++ client/src/components/Comments/Comment.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// Component Import
+import AuthorSmall from '../AuthorSmall'
 
 export default function Comment({ comment }) {
     const nestedComments = (comment.children || []).map((comment) => {
@@ -8,6 +10,7 @@ export default function Comment({ comment }) {
   
     return (
       <div className={comment.lineage ? 'comment childComment' : 'comment parentComment'}>
+        <AuthorSmall />
         <div className='commentText'>{`${comment.comment} · ${comment.user_name}`}</div>
         {nestedComments}
       </div>
