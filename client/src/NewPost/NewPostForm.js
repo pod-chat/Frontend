2e31d9f8efc9c5f7b7cddb37e4228378487284f2
ClintFix <clint.fix@gmail.com>
2021-04-29 11:55:37 -0600

    form built

--- client/src/NewPost/NewPostForm.js
+++ client/src/NewPost/NewPostForm.js
@@ -11,13 +11,22 @@ import { useForm } from 'react-hook-form';
 // }
 
 export default function NewPostForm() {
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>Post Title</label>
-            <input {...register("post_title")} />
-        </form>
+        <div>
+            <div>Create a New Podcast Clip!</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Post Title</label>
+                <input {...register("post_title", { required: true, maxLength: 250 })} />
+                {errors.post_title && "Title is required"}
+                <label>Tell Us More</label>
+                <input {...register("post_body", { required: true, maxLength: 1000 })} />
+                {errors.post_body && "Gotta tell us something about this post"}
+
+                <input type="submit" />
+            </form>
+        </div>
     )
 }
